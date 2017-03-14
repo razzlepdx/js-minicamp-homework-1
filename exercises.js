f02315eb2e9c4ad25c84aadcbf0cb531f5932648
Rachel Wilson <razzlepdx@users.noreply.github.com>
2017-03-13 21:59:14 -0700

    update with homework answers

--- exercises.js
+++ exercises.js
@@ -3,137 +3,177 @@
 function multiplyByTen(num) {
   //return num after multiplying it by ten
   //code here
+  var product = num * 10;
+  return product;
 }
 
 function subtractFive(num) {
   //return num after subtracting five
   //code here
+  var minusFive = num - 5;
+  return minusFive;
 }
 
 function areSameLength(str1, str2) {
   //return true if the two strings have the same length
   //otherwise return false
   //code here
+  return str1.length === str2.length;
 }
 
 function areEqual(x, y) {
   //return true if x and y are the same
   //otherwise return false
   //code here
+  return x === y;
 }
 
 function lessThanNinety(num) {
   //return true if num is less than ninety
   //otherwise return false
   //code here
+  return num < 90;
 }
 
 function greaterThanFifty(num) {
   //return true if num is greater than fifty
   //otherwise return false
   //code here
+  return num > 50;
 }
 
 function add(x, y) {
   //add x and y together and return the value
   //code here
+  var sum = x + y;
+  return sum;
 }
 
 function subtract(x, y) {
   //subtract y from x and return the value
   //code here
+  var difference = x - y;
+  return difference;
 }
 
 function divide(x, y) {
   //divide x by y and return the value
   //code here
+  var quotient = x / y;
+  return quotient;
 }
 
 function multiply(x, y) {
   //multiply x by y and return the value
   //code here
+  var product = x * y;
+  return product;
 }
 
 function getRemainder(x, y) {
   //return the remainder from dividing x by y
   //code here
+  var remainder = x % y;
+  return remainder;
 }
 
 function isEven(num) {
   //return true if num is even
   //otherwise return false
   //code here
+  return num % 2 === 0;
 }
 
 function isOdd(num) {
   //return true if num is false
   //otherwise return false
   //code here
+  return (num % 2) > 0;
 }
 
 function square(num) {
   //square num and return the new value
   //code here
+  var squared = Math.pow(num, 2);
+  return squared;
 }
 
 function cube(num) {
   //cube num and return the new value
   //code here
+  var cubed = Math.pow(num, 3);
+  return cubed;
 }
 
 function raiseToPower(num, exponent) {
   //raise num to whatever power is passed in as exponent
   //code here
+  var poweredUp = Math.pow(num, exponent);
+  return poweredUp;
 }
 
 function roundNumber(num) {
   //round num and return it
   //code here
+  var rounded = Math.round(num);
+  return rounded;
 }
 
 function roundUp(num) {
   //round num up and return it
   //code here
+  var roundedUp = Math.ceil(num);
+  return roundedUp;
 }
 
 function addExclamationPoint(str) {
   //add an exclamation point to the end of str and return the new string
   //'hello world' -> 'hello world!'
   //code here
+  var exclaimed = str + '!';
+  return exclaimed;
 }
 
 function combineNames(firstName, lastName) {
   //return firstName and lastName combined as one string and separated by a space.
   //'Lambda', 'School' -> 'Lambda School'
   //code here
+
+  return firstName + ' ' + lastName;
 }
 
 function getGreeting(name) {
   //Take the name string and concatenate other strings onto it so it takes the following form:
   //'Sam' -> 'Hello Sam!'
   //code here
+  return 'Hello ' + name + '!';
 }
 
 //If you can't remember these area formulas then head over to Google or look at the test code.
- 
+
 function getRectangleArea(length, width) {
   //return the area of the rectangle by using length and width
   //code here
+  return length * width;
 }
 
 function getTriangleArea(base, height) {
   //return the area of the triangle by using base and height
   //code here
+  return (base * height) / 2;
 }
 
 function getCircleArea(radius) {
   //return the rounded area of the circle given the radius
   //code here
+ var area = Math.pow(radius, 2) * Math.PI;
+ return Math.round(area);
 }
 
 function getRectangularPrismVolume(length, width, height) {
   //return the area of the 3D rectangular prism given the length, width, and height
   //code here
+  return length * width * height;
 }
 
 //Do not modify code below this line.
